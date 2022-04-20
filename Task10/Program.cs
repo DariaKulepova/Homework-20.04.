d41c45d1da761b1d478b6144f5a93486ead968a2
Daria <daria.kulepova@gmail.com>
2022-04-20 20:22:12 +0300

    Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ using System;
+
+int num=new Random().Next(100,1000);
+/*string numS=num.ToString();
+Console.WriteLine($"{num}->{numS[1]}");*/
+int num2=num%100/10;
+Console.WriteLine($"{num}->{num2}");
