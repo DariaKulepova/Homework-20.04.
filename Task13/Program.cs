1468fe72ad45c0b61152407c9903cd58e89037c0
Daria <daria.kulepova@gmail.com>
2022-04-21 06:32:23 +0300

    Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,23 +1,8 @@
 ﻿
- /*using System;
-num=new Random().Next(100,1000);
-/*Console.WriteLine("Input number");
-int num=Convert.ToInt32(Console.ReadLine());*/
-/*string numS=num.ToString();
-Console.WriteLine($"{num[2]}");*/
-/*if(num>=100)
-{
-    
-}
-else
-{
-    Console.WriteLine("Третьего числа нет");
-}*/
-
 using System;
 
 Console.WriteLine("Input number");
-int num=int.Parse(Console.ReadLine());
+int num=Convert.ToInt32(Console.ReadLine());
 string numS=num.ToString();
 if(num>=100)
 {
@@ -27,3 +12,4 @@ else
 {
     Console.WriteLine("Третьей цифры не существует");
 }
+
